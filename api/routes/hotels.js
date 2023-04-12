357af487d6f6d08a0c4d79a101f07bd0859bbe99
shajiehhh <huang.shaoj@northeastern.edu>
2023-04-12 10:44:18 -0700

    utils

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -9,7 +9,6 @@ import {
     getHotels,
     updateHotel,
 } from "../controllers/hotel.js";
-import Hotel from "../models/Hotel.js";
 import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
@@ -18,13 +17,14 @@ router.post("/", verifyAdmin, createHotel);
 
 //UPDATE
 router.put("/:id", verifyAdmin, updateHotel);
+
 //DELETE
 router.delete("/:id", verifyAdmin, deleteHotel);
+
 //GET
-
 router.get("/find/:id", getHotel);
-//GET ALL
 
+//GET ALL
 router.get("/", getHotels);
 router.get("/countByCity", countByCity);
 router.get("/countByType", countByType);
